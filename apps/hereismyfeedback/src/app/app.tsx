9d3ad4a1fb507c017c3326ac66f8e76557aba541
Sebastian Walter <sebastian.walter@hekori.com>
2021-05-09 20:34:43 +0200

    integrated tailwind

--- apps/hereismyfeedback/src/app/app.tsx
+++ apps/hereismyfeedback/src/app/app.tsx
@@ -1,104 +1,219 @@
-import styles from "./app.module.css";
-
-import { ReactComponent as Logo } from "./logo.svg";
-import star from "./star.svg";
 import { useGlobal } from "../GlobalProvider";
 
 export function App() {
   const { connect, accessToken } = useGlobal();
   connect(accessToken);
+  // return <div className={styles.app}>test</div>;
   return (
-    <div className={styles.app}>
-      <header className="flex">
-        <Logo width="75" height="75" />
-        <h1>Welcome to hereismyfeedback!</h1>
-      </header>
-      <main>
-        <h2>Resources &amp; Tools</h2>
-        <p>Thank you for using and showing some ♥ for Nx.</p>
-        <div className="flex github-star-container">
-          <a
-            href="https://github.com/nrwl/nx"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            If you like Nx, please give it a star:
-            <div className="github-star-badge">
-              <img src={star} className="material-icons" alt="" />
-              Star
+    <div className="min-h-screen bg-gray-100 p-0 sm:p-12">
+      <div className="mx-auto max-w-md px-6 py-12 bg-white border-0 shadow-lg sm:rounded-3xl">
+        <h1 className="text-2xl font-bold mb-8">Form With Floating Labels</h1>
+        <form id="form" noValidate>
+          <div className="relative z-0 w-full mb-5">
+            <input
+              type="text"
+              name="name"
+              placeholder=" "
+              required
+              className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+            />
+            <label
+              htmlFor="name"
+              className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500"
+            >
+              Enter name
+            </label>
+            <span className="text-sm text-red-600 hidden" id="error">
+              Name is required
+            </span>
+          </div>
+
+          <div className="relative z-0 w-full mb-5">
+            <input
+              type="email"
+              name="email"
+              placeholder=" "
+              className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+            />
+            <label
+              htmlFor="email"
+              className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500"
+            >
+              Enter email address
+            </label>
+            <span className="text-sm text-red-600 hidden" id="error">
+              Email address is required
+            </span>
+          </div>
+
+          <div className="relative z-0 w-full mb-5">
+            <input
+              type="password"
+              name="password"
+              placeholder=" "
+              className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+            />
+            <label
+              htmlFor="password"
+              className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500"
+            >
+              Enter password
+            </label>
+            <span className="text-sm text-red-600 hidden" id="error">
+              Password is required
+            </span>
+          </div>
+
+          <fieldset className="relative z-0 w-full p-px mb-5">
+            <legend className="absolute text-gray-500 transform scale-75 -top-3 origin-0">
+              Choose an option
+            </legend>
+            <div className="block pt-3 pb-2 space-x-4">
+              <label>
+                <input
+                  type="radio"
+                  name="radio"
+                  value="1"
+                  className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+                />
+                Option 1
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="radio"
+                  value="2"
+                  className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+                />
+                Option 2
+              </label>
             </div>
-          </a>
-        </div>
-        <p>Here are some links to help you get started.</p>
-        <ul className="resources">
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://egghead.io/playlists/scale-react-development-with-nx-4038"
+            <span className="text-sm text-red-600 hidden" id="error">
+              Option has to be selected
+            </span>
+          </fieldset>
+
+          <div className="relative z-0 w-full mb-5">
+            <select
+              name="select"
+              value=""
+              onClick={() => {
+                console.log("clicked");
+              }}
+              className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none z-1 focus:outline-none focus:ring-0 focus:border-black border-gray-200"
             >
-              Scale React Development with Nx (Course)
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/latest/react/tutorial/01-create-application"
+              <option value="" selected disabled hidden></option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+              <option value="4">Option 4</option>
+              <option value="5">Option 5</option>
+            </select>
+            <label
+              htmlFor="select"
+              className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500"
             >
-              Interactive tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a className="resource flex" href="https://nx.app/">
-              <svg
-                width="36"
-                height="36"
-                viewBox="0 0 120 120"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+              Select an option
+            </label>
+            <span className="text-sm text-red-600 hidden" id="error">
+              Option has to be selected
+            </span>
+          </div>
+
+          <div className="flex flex-row space-x-4">
+            <div className="relative z-0 w-full mb-5">
+              <input
+                type="text"
+                name="date"
+                placeholder=" "
+                onClick={() => {
+                  console.log("clicked");
+                }}
+                className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+              />
+              <label
+                htmlFor="date"
+                className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500"
               >
-                <path
-                  d="M120 15V30C103.44 30 90 43.44 90 60C90 76.56 76.56 90 60 90C43.44 90 30 103.44 30 120H15C6.72 120 0 113.28 0 105V15C0 6.72 6.72 0 15 0H105C113.28 0 120 6.72 120 15Z"
-                  fill="#0E2039"
-                />
-                <path
-                  d="M120 30V105C120 113.28 113.28 120 105 120H30C30 103.44 43.44 90 60 90C76.56 90 90 76.56 90 60C90 43.44 103.44 30 120 30Z"
-                  fill="white"
-                />
-              </svg>
-              <span className="gutter-left">Nx Cloud</span>
-            </a>
-          </li>
-        </ul>
-        <h2>Next Steps</h2>
-        <p>Here are some things you can do with Nx.</p>
-        <details open>
-          <summary>Add UI library</summary>
-          <pre>{`# Generate UI lib
-nx g @nrwl/react:lib ui
+                Date
+              </label>
+              <span className="text-sm text-red-600 hidden" id="error">
+                Date is required
+              </span>
+            </div>
+            <div className="relative z-0 w-full">
+              <input
+                type="text"
+                name="time"
+                placeholder=" "
+                onClick={() => {
+                  console.log("clicked");
+                }}
+                className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+              />
+              <label
+                htmlFor="time"
+                className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500"
+              >
+                Time
+              </label>
+              <span className="text-sm text-red-600 hidden" id="error">
+                Time is required
+              </span>
+            </div>
+          </div>
 
-# Add a component
-nx g @nrwl/react:component xyz --project ui`}</pre>
-        </details>
-        <details>
-          <summary>View dependency graph</summary>
-          <pre>{`nx dep-graph`}</pre>
-        </details>
-        <details>
-          <summary>Run affected commands</summary>
-          <pre>{`# see what's been affected by changes
-nx affected:dep-graph
+          <div className="relative z-0 w-full mb-5">
+            <input
+              type="number"
+              name="money"
+              placeholder=" "
+              className="pt-3 pb-2 pl-5 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+            />
+            <div className="absolute top-0 left-0 mt-3 ml-1 text-gray-400">
+              $
+            </div>
+            <label
+              htmlFor="money"
+              className="absolute duration-300 top-3 left-5 -z-1 origin-0 text-gray-500"
+            >
+              Amount
+            </label>
+            <span className="text-sm text-red-600 hidden" id="error">
+              Amount is required
+            </span>
+          </div>
 
-# run tests for current changes
-nx affected:test
+          <div className="relative z-0 w-full mb-5">
+            <input
+              type="text"
+              name="duration"
+              placeholder=" "
+              className="pt-3 pb-2 pr-12 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+            />
+            <div className="absolute top-0 right-0 mt-3 mr-4 text-gray-400">
+              min
+            </div>
+            <label
+              htmlFor="duration"
+              className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500"
+            >
+              Duration
+            </label>
+            <span className="text-sm text-red-600 hidden" id="error">
+              Duration is required
+            </span>
+          </div>
 
-# run e2e tests for current changes
-nx affected:e2e
-  `}</pre>
-        </details>
-      </main>
+          <button
+            id="button"
+            type="button"
+            className="w-full px-6 py-3 mt-3 text-lg text-white transition-all duration-150 ease-linear rounded-lg shadow outline-none bg-pink-500 hover:bg-pink-600 hover:shadow-lg focus:outline-none"
+          >
+            Toggle Error
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const nrwlConfig = require("@nrwl/react/plugins/webpack.js");
+
+module.exports = (config) => {
+  nrwlConfig(config);
+  return {
+    ...config,
+    module: {
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.css$|\.scss$|\.sass$|\.less$|\.styl$/,
+          use: [
+            {
+              loader: "postcss-loader",
+            },
+          ],
+        },
+      ],
+    },
+  };
+};
