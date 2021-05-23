1d950222fae1ffcf554c211706b7b363f49fbcb9
Sebastian Walter <sebastian.walter@hekori.com>
2021-05-23 10:39:57 +0200

    renamed api to hereismyfeedback-ai

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/hereismyfeedback',"<rootDir>/apps/api","<rootDir>/libs/shared","<rootDir>/libs/uikit","<rootDir>/libs/cli"],
-};
+    projects: [
+        '<rootDir>/apps/hereismyfeedback',
+        '<rootDir>/libs/shared',
+        '<rootDir>/libs/uikit',
+        '<rootDir>/libs/cli',
+        '<rootDir>/apps/hereismyfeedback-api',
+    ],
+}
