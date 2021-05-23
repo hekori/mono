6131b5d5185a695d34692bfdab8dda8e05ed29be
Sebastian Walter <sebastian.walter@hekori.com>
2021-05-23 13:51:46 +0200

    cleanup folder structure and env vars

--- apps/hereismyfeedback-api/src/main.ts
+++ apps/hereismyfeedback-api/src/main.ts
@@ -1,53 +1,54 @@
-import express = require("express");
-const app = express();
-import http = require("http");
-const server = http.createServer(app);
-import { Server } from "socket.io";
-import jwt = require("jsonwebtoken");
+import express = require('express')
+const app = express()
+import http = require('http')
+const server = http.createServer(app)
+import { Server } from 'socket.io'
+import jwt = require('jsonwebtoken')
+import { environment } from './environments/environment'
 
-console.log("NX_JWT_PRIVATE_KEY=", process.env.NX_JWT_PRIVATE_KEY);
-console.log(jwt.sign("haha", process.env.NX_JWT_PRIVATE_KEY));
+console.log('NX_JWT_PRIVATE_KEY=', process.env.NX_JWT_PRIVATE_KEY)
+console.log(jwt.sign('haha', process.env.NX_JWT_PRIVATE_KEY))
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+})
 
 io.use(function (socket, next) {
-  console.log(socket.handshake.query);
-  if (socket.handshake.query && socket.handshake.query.accessToken) {
-    try {
-      setInterval(() => {
-        socket.emit("news", { hello: "world" });
-      }, 1000);
+    console.log(socket.handshake.query)
+    if (socket.handshake.query && socket.handshake.query.accessToken) {
+        try {
+            setInterval(() => {
+                socket.emit('news', { hello: 'world' })
+            }, 1000)
 
-      const decoded = jwt.verify(
-        socket.handshake.query.accessToken,
-        process.env.NX_JWT_PRIVATE_KEY
-      );
-      console.log("decoded=", decoded);
-    } catch (e) {
-      console.log("error in verification");
-      return next(new Error("Authentication error"));
+            const decoded = jwt.verify(
+                socket.handshake.query.accessToken,
+                process.env.NX_JWT_PRIVATE_KEY
+            )
+            console.log('decoded=', decoded)
+        } catch (e) {
+            console.log('error in verification')
+            return next(new Error('Authentication error'))
+        }
+    } else {
+        console.log('error')
+        next(new Error('Authentication error'))
     }
-  } else {
-    console.log("error");
-    next(new Error("Authentication error"));
-  }
-  next();
-});
+    next()
+})
 
-io.on("connection", function (socket) {
-  // Connection now authenticated to receive further events
-  console.log("a user connected");
+io.on('connection', function (socket) {
+    // Connection now authenticated to receive further events
+    console.log('a user connected')
 
-  socket.on("message", function (message) {
-    io.emit("message", message);
-  });
-});
+    socket.on('message', function (message) {
+        io.emit('message', message)
+    })
+})
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
-});
+server.listen(environment.port, () => {
+    console.log(`listening on *:${environment.port}`)
+})
