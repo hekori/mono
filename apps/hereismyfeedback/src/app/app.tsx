032220a6bdb3d8e5476eb4e29cd7fa558139572c
Sebastian Walter <sebastian.walter@hekori.com>
2021-05-23 14:49:19 +0200

    started scaffolding hereismyfeedback

--- apps/hereismyfeedback/src/app/app.tsx
+++ apps/hereismyfeedback/src/app/app.tsx
@@ -1,13 +1,8 @@
 import { useGlobal } from '../GlobalProvider'
-
-import {
-    ButtonFlat,
-    Typography,
-    Select,
-    NoShell,
-    Card,
-    Input,
-} from '@hekori/uikit'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { LandingPage } from './pages/LandingPage'
+import { CreateNewFeedback } from './pages/CreateNewFeedback'
+import { IndexPage } from './pages/IndexPage'
 
 export function App() {
     const { connect, accessToken } = useGlobal()
@@ -17,32 +12,18 @@ export function App() {
     })
 
     return (
-        <NoShell>
-            <Card>
-                <Typography type="h1">Form With Floating Labels</Typography>
-                <form id="form" noValidate>
-                    <div className="relative z-0 w-full mb-5">
-                        <Input
-                            placeholder={'Enter name'}
-                            label={'some label'}
-                        />
-                        <Input type="email" placeholder={'Enter email'} />
-                        <Input
-                            type="password"
-                            name="password"
-                            placeholder="Enter password"
-                        />
-                        <Select
-                            options={[
-                                { value: '1', label: '1' },
-                                { value: '2', label: '2' },
-                            ]}
-                        />
-                    </div>
-
-                    <ButtonFlat text={'Submit'} />
-                </form>
-            </Card>
-        </NoShell>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <IndexPage />
+                </Route>
+                <Route path="/landing" exact>
+                    <LandingPage />
+                </Route>
+                <Route path="/create" exact>
+                    <CreateNewFeedback />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     )
 }
