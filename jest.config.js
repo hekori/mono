121ae136cd4f5bafe5516533665f350043c66d4c
Sebastian Walter <sebastian.walter@hekori.com>
2021-05-24 18:22:48 +0200

    started working on tailwind css

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    projects: [
-        '<rootDir>/apps/hereismyfeedback',
-        '<rootDir>/libs/traqrcode-common',
-        '<rootDir>/libs/uikit',
-        '<rootDir>/libs/cli',
-        '<rootDir>/apps/hereismyfeedback-api',
-        '<rootDir>/libs/mail',
-        '<rootDir>/apps/traqrcode',
-        '<rootDir>/apps/traqrcode-api',
-    ],
+  projects: [
+    '<rootDir>/apps/hereismyfeedback',
+    '<rootDir>/libs/traqrcode-common',
+    '<rootDir>/libs/uikit',
+    '<rootDir>/libs/cli',
+    '<rootDir>/apps/hereismyfeedback-api',
+    '<rootDir>/libs/mail',
+    '<rootDir>/apps/traqrcode',
+    '<rootDir>/apps/traqrcode-api',
+  ],
 }
