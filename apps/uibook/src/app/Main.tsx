a6e9f9818b032f050c160099e619115605a4e0dd
Sebastian Walter <sebastian.walter@hekori.com>
2021-05-28 16:34:54 +0200

    cleanup

--- apps/uibook/src/app/Main.tsx
+++ apps/uibook/src/app/Main.tsx
@@ -1,12 +1,10 @@
-import { Route, Switch, useHistory, useLocation } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import * as React from 'react'
 import { ButtonStory } from './10_ButtonStory'
 import { TypographyStory } from './01_TypographyStory'
 import { ColorStory } from './02_ColorStory'
 
 export const Main: React.FC = ({ children }) => {
-  const location = useLocation()
-
   return (
     <div className="py-4">
       <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
