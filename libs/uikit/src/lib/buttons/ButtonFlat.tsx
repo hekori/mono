62087893f17643fc038a15255c6064211f85df6b
Sebastian Walter <sebastian.walter@hekori.com>
2021-08-22 13:45:15 +0200

    improved PageDetails responsive layout

--- libs/uikit/src/lib/buttons/ButtonFlat.tsx
+++ libs/uikit/src/lib/buttons/ButtonFlat.tsx
@@ -9,7 +9,7 @@ export const ButtonFlat: React.FC<ButtonProps> = ({ children, ...props }) => {
       {...props}
       //focus:outline-none focus:ring-1 focus:ring-offset-1
       className={classNames(
-        'cursor-pointer px-8 py-4 flex justify-center font-medium rounded-md text-onDocument hover:text-onDocumentHighlight focus:text-onDocument focus:outline-none',
+        'cursor-pointer py-4 px-4 flex items-center justify-center font-medium rounded-md text-onDocument hover:text-onDocumentHighlight focus:text-onDocument focus:outline-none',
         props.className
       )}
     >
