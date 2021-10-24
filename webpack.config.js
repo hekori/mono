79946959c539f3c64cc271b9818851cd2ddc036b
Sebastian Walter <sebastian.walter@hekori.com>
2021-10-24 18:57:51 +0200

    move all traqrcode files to common directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
-const nrwlConfig = require("@nrwl/react/plugins/webpack.js");
+const nrwlConfig = require('@nrwl/react/plugins/webpack.js')
 
 module.exports = (config) => {
-  nrwlConfig(config);
+  nrwlConfig(config)
   return {
     ...config,
     module: {
       rules: [
         ...config.module.rules,
         {
+          test: /\.svg$/,
+          use: ['@svgr/webpack', 'url-loader'],
+        },
+        {
           test: /\.css$|\.scss$|\.sass$|\.less$|\.styl$/,
           use: [
             {
-              loader: "postcss-loader",
+              loader: 'postcss-loader',
             },
           ],
         },
       ],
     },
-  };
-};
+  }
+}
