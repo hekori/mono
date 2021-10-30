8daba39ea8447cfceb51bf21f15bbd3f1286be4d
Sebastian Walter <sebastian.walter@hekori.com>
2021-10-30 21:07:05 +0200

    towards a working deployment

--- projects/traqrcode/traqrcode-deploy/flightplan.ts
+++ projects/traqrcode/traqrcode-deploy/flightplan.ts
@@ -5,15 +5,18 @@ import * as fs from 'fs'
 import * as Handlebars from 'handlebars'
 
 import { config } from 'dotenv'
+import * as path from 'path'
 
-const DOT_ENV_PATH = `${__dirname}/../traqrcode-env/traqrcode-api--prod.txt`
+const DOT_ENV_PATH = path.join(
+  __dirname,
+  '../traqrcode-env/traqrcode--prod.txt'
+)
 
 config({ path: DOT_ENV_PATH })
-console.log(`loaded ${DOT_ENV_PATH}`)
-throw new Error()
+
+console.log(`loaded\n${DOT_ENV_PATH}`)
 
 const DEPLOY_HOST = process.env.DEPLOY_HOST || 'DEPLOY_HOST'
-
 const FRONTEND_URL = process.env.FRONTEND_URL || 'FRONTEND_URL'
 const BACKEND_URL = process.env.BACKEND_URL || 'BACKEND_URL'
 
@@ -29,7 +32,6 @@ const HOST_SLUG =
   process.env.HOST_SLUG || slugify(FRONTEND_HOST, { remove: /[*+~.()'"!:@]/g })
 
 const PROTOCOLS = ['http', 'https']
-const PKGS = ['frontend', 'backend']
 
 const PM2_NAME = `api_${HOST_SLUG}`
 
@@ -39,6 +41,7 @@ const ROOT_DIR = process.env.ROOT_DIR
 // console.log(ROOT_DIR)
 
 console.log(`FRONTEND_URL = ${FRONTEND_URL}`)
+console.log(`BACKEND_URL = ${BACKEND_URL}`)
 console.log(`FRONTEND_HOST = ${FRONTEND_HOST}`)
 console.log(`BACKEND_HOST = ${BACKEND_HOST}`)
 console.log(`SSL_CERTIFICATE_PATH_CHAINED = ${SSL_CERTIFICATE_PATH_CHAINED}`)
@@ -71,7 +74,10 @@ flightplan.local('deploy', (local) => {
   // })
 
   local.exec(`git push ${HOST_SLUG} master`)
-  local.transfer(['./.env.prod'], `${ROOT_DIR}/webapp/`)
+
+  local.with(`cd ${path.dirname(DOT_ENV_PATH)}`, () => {
+    local.transfer('traqrcode--prod.txt', `${ROOT_DIR}`)
+  })
 })
 
 // run commands on remote
@@ -79,19 +85,15 @@ flightplan.remote('deploy', (remote) => {
   remote.log('Make dir')
   remote.exec('pwd')
   remote.with(`cd ${ROOT_DIR}/webapp`, () => {
-    remote.exec('mv .env.prod .env')
-
     remote.exec(`git pull`)
     remote.exec('yarn install --frozen-lockfile')
 
-    remote.with('cd packages/frontend', () => {
-      const TARGET_DIR = `${ROOT_DIR}/assets/available/${getNow()}`
-      remote.exec('yarn clean')
-      remote.exec('yarn build')
-      remote.exec(`cp -r dist ${TARGET_DIR}`)
-      remote.exec(`rm -f ${ROOT_DIR}/assets/enabled`)
-      remote.exec(`ln -s ${TARGET_DIR} ${ROOT_DIR}/assets/enabled`)
-    })
+    const TARGET_DIR = `${ROOT_DIR}/assets/available/${getNow()}`
+    remote.exec('yarn clean')
+    remote.exec('yarn traqrcode:build:prod:frontend')
+    remote.exec(`cp -r dist/projects/traqrcode-frontend ${TARGET_DIR}`)
+    remote.exec(`rm -f ${ROOT_DIR}/assets/enabled`)
+    remote.exec(`ln -s ${TARGET_DIR} ${ROOT_DIR}/assets/enabled`)
   })
 
   remote.exec('service nginx stop')
@@ -116,6 +118,8 @@ flightplan.remote('provision', (remote) => {
     'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
   )
   remote.exec('locale-gen en_US.UTF-8')
+
+  remote.exec('curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -')
 
   remote.exec('apt-get update')
   remote.exec(
@@ -150,36 +154,40 @@ flightplan.local('provision', (local) => {
   local.exec('mkdir -p ./tmp/provision')
 
   for (const protocol of PROTOCOLS) {
-    for (const pkg of PKGS) {
-      const nginx_conf = fs.readFileSync(
-        `${__dirname}/misc/installation/nginx.${protocol}.${pkg}.template.txt`,
-        'utf8'
-      )
-      const template = Handlebars.compile(nginx_conf)
-      const nginx_conf_replaced = template({
-        HOST_SLUG,
-        BACKEND_HOST,
-        FRONTEND_HOST,
-        SSL_CERTIFICATE_PATH_CHAINED,
-        SSL_CERTIFICATE_PATH_PRIVKEY,
-      })
-      const nginxConfFileName = `${HOST_SLUG}_${protocol}_${pkg}`
+    const nginx_conf = fs.readFileSync(
+      `${__dirname}/installation/nginx.${protocol}.template.txt`,
+      'utf8'
+    )
+    const template = Handlebars.compile(nginx_conf)
+    const nginx_conf_replaced = template({
+      HOST_SLUG,
+      BACKEND_HOST,
+      FRONTEND_HOST,
+      SSL_CERTIFICATE_PATH_CHAINED,
+      SSL_CERTIFICATE_PATH_PRIVKEY,
+    })
+    const nginxConfFileName = `${HOST_SLUG}_${protocol}`
 
-      fs.writeFileSync(
-        `./tmp/provision/${nginxConfFileName}`,
-        nginx_conf_replaced
-      )
-
-      local.with(`cd ./tmp/provision/`, () => {
-        local.transfer([`${nginxConfFileName}`], '/etc/nginx/sites-available')
-      })
-    }
+    fs.writeFileSync(
+      `./tmp/provision/${nginxConfFileName}`,
+      nginx_conf_replaced
+    )
+    local.with(`cd ./tmp/provision/`, () => {
+      local.transfer([`${nginxConfFileName}`], '/etc/nginx/sites-available')
+    })
   }
 
   local.log('Push git repo to remote')
   local.exec(`git remote rm ${HOST_SLUG}`)
   local.exec(`git remote add ${HOST_SLUG} root@${DEPLOY_HOST}:${ROOT_DIR}/git`)
   local.exec(`git push -u ${HOST_SLUG} master`)
+
+  local.log(`Push DOT_ENV file ${DOT_ENV_PATH}`)
+  local.exec(`ls -lah ${DOT_ENV_PATH}`)
+
+  local.with(`cd ${path.dirname(DOT_ENV_PATH)}`, () => {
+    local.transfer('traqrcode--prod.txt', `${ROOT_DIR}`)
+  })
 })
 
 flightplan.remote('provision', (remote) => {
@@ -192,18 +200,18 @@ flightplan.remote('provision', (remote) => {
   remote.exec('rm -f /etc/nginx/sites-enabled/default')
 
   for (const protocol of PROTOCOLS) {
-    for (const pkg of PKGS) {
-      const nginxConfFileName = `${HOST_SLUG}_${protocol}_${pkg}`
-      remote.exec(
-        `ln -fs /etc/nginx/sites-available/${nginxConfFileName} /etc/nginx/sites-enabled/${nginxConfFileName}`
-      )
-    }
+    const nginxConfFileName = `${HOST_SLUG}_${protocol}`
+    remote.exec(
+      `ln -fs /etc/nginx/sites-available/${nginxConfFileName} /etc/nginx/sites-enabled/${nginxConfFileName}`
+    )
   }
 
   remote.log('Setup pm2')
-  remote.with(`cd ${ROOT_DIR}/webapp/packages/backend`, () => {
+  remote.with(`cd ${ROOT_DIR}/webapp`, () => {
     remote.exec('pm2 delete all || true')
-    remote.exec(`pm2 start "yarn start:prod" --name "${PM2_NAME}"`)
+    remote.exec(
+      `pm2 start "yarn traqrcode:serve:prod:backend" --name "${PM2_NAME}"`
+    )
     remote.exec(`pm2 save`)
     remote.exec(`pm2 startup`)
   })
