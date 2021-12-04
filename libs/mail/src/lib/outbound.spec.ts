2debbc591a1e2d415fceb0e08357dfea33890a73
Sebastian Walter <sebastian.walter@hekori.com>
2021-12-04 17:51:43 +0100

    removed libs/mail (was an empty library)

--- libs/mail/src/lib/outbound.spec.ts
+++
@@ -1,7 +0,0 @@
-import { outbound } from './outbound'
-
-describe('mail', () => {
-  it('should work', () => {
-    expect(outbound()).toEqual('mail')
-  })
-})
--- libs/mail/src/lib/outbound.ts
+++
@@ -1,3 +0,0 @@
-export function outbound(): string {
-  return 'mail'
-}
