f016762abd30e458b8421e9c455643f538ce0390
Sebastian Walter <sebastian.walter@hekori.com>
2022-02-06 20:02:24 +0100

    fix traqrcode:e2e

--- projects/traqrcode/traqrcode-e2e/src/integration/app.spec.ts
+++ projects/traqrcode/traqrcode-e2e/src/integration/app.spec.ts
@@ -1,13 +1,13 @@
-import { getGreeting } from '../support/app.po'
+import { getCreateQRCodeButton } from '../support/app.po'
 
 describe('traqrcode', () => {
-    beforeEach(() => cy.visit('/'))
+  beforeEach(() => cy.visit('/'))
 
-    it('should display welcome message', () => {
-        // Custom command example, see `../support/commands.ts` file
-        cy.login('my-email@something.com', 'myPassword')
+  it('should display the frontpage', () => {
+    // Custom command example, see `../support/commands.ts` file
+    // cy.login('my-email@something.com', 'myPassword')
 
-        // Function helper example, see `../support/app.po.ts` file
-        getGreeting().contains('Welcome to traqrcode!')
-    })
+    // Function helper example, see `../support/app.po.ts` file
+    getCreateQRCodeButton().contains('Create QR Code')
+  })
 })
