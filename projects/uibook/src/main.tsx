4e7e05d9f9ccaf25a1d0a75046d8140c01fdca3b
Sebastian Walter <sebastian.walter@hekori.com>
2022-02-12 17:43:46 +0100

    moved uibook to projects

--- projects/uibook/src/main.tsx
+++ projects/uibook/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import 'tailwindcss/tailwind.css'
 
 import { Shell } from './app/Shell'
 
